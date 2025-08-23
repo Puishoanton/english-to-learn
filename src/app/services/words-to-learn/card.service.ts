eb838c209bb5fa6f78ae418bb5f013bee83d8894
anton-puisho <puishoanton@gmail.com>
2025-08-23 18:55:13 +0200

    feat: “I know” and “Study again” buttons change word progress

--- src/app/services/words-to-learn/card.service.ts
+++ src/app/services/words-to-learn/card.service.ts
@@ -26,6 +26,16 @@ export class CardService {
       createdAt: new Date(),
       updatedAt: new Date(),
       deckId: '1'
+    },
+    {
+      id: '3',
+      word: '3 test 3',
+      word_context: '3 We need to test this hypothesis. 3',
+      translation: '3 тест 3',
+      translation_context: '3 перевірка гіпотези 3',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      deckId: '1'
     }
   ]
   private readonly httpClient = inject(HttpClient);
@@ -47,5 +57,10 @@ export class CardService {
   public deleteCard(cardId: string) {
     console.log(cardId);
     return { status: 200, message: 'Deleted' }
+  }
+
+  public changeProgress(cardId: string, changeValue: 'decrease' | 'increase') {
+    console.log(cardId, changeValue);
+    return { status: 200, }
   }
 }
