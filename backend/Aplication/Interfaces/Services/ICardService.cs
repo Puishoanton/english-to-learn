fb595121c40b96cf93001aaf5b811015a289517e
anton-puisho <puishoanton@gmail.com>
2025-08-25 20:28:51 +0200

    feat: Backend | Create a cards endpoint

---
+++ backend/Aplication/Interfaces/Services/ICardService.cs
@@ -0,0 +1,14 @@
+﻿using EnglishToLearn.Aplication.DTOs.Card;
+using EnglishToLearn.Domain.Entities;
+
+namespace EnglishToLearn.Aplication.Interfaces.Services
+{
+    public interface ICardService
+    {
+        Task<Card?> GetCardByIdAsync(Guid id);
+        Task<ICollection<Card>> GetAllCards();
+        Task AddCardAsync(Card card);
+        Task UpdateCardAsync(Guid id, UpdateCardDto updateCardDto);
+        Task DeleteCardAsync(Guid id);
+    }
+}
