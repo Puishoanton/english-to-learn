f8af6bed803453f0a2353f49dbc1a6f24269a303
anton-puisho <puishoanton@gmail.com>
2025-09-02 13:33:44 +0200

    feat: Frontend | Deck name as a main header

--- frontend/src/app/services/navigation.service.ts
+++ frontend/src/app/services/navigation.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { DeckService } from './words-to-learn/deck.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavigationService {
+  private readonly deckService = inject(DeckService)
+
   private readonly menuItems: MenuItem[] = [
     { label: 'Home', icon: 'pi pi-home', routerLink: '/' },
     { label: 'Words to learn', icon: 'pi pi-book', routerLink: '/words-to-learn' },
@@ -15,10 +18,20 @@ export class NavigationService {
   }
   public getLabelByEndpoint(endpoint: string): string {
     const label = this.menuItems.find(item => item.routerLink === endpoint && endpoint !== '/')?.label
+    const deckId = this.getDeckId(endpoint)
+
+    if (deckId) {
+      return this.deckService.decks().find(deck => deck.id === deckId)?.name ?? 'English to learn'
+    }
 
     if (label) {
       return label
     }
     return 'English to learn'
+  }
+  private getDeckId(endpoint: string): string | null {
+    const regex = /^\/words-to-learn\/deck\/([0-9a-fA-F-]+)(?:\/study)?$/;
+    const match = endpoint.match(regex);
+    return match ? match[1] : null;
   }
 }
